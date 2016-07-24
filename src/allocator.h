1c88636583b2e6a44e05b023e291747018cc690b
Lee Avital <leeavital@gmail.com>
2016-07-23 23:02:22 -0400

    Add docs

--- src/allocator.h
+++ src/allocator.h
@@ -10,12 +10,27 @@ struct lgc_object_t {
   int out_reference_count; //this is effectively how many elements are in objects
 };
 
+/**
+ * allocator an lgc_object_t on the heap. The allocated memory can be safely freed
+ * using lgc_free.
+ */
 lgc_object_t lgc_allocate(int size);
 
+/**
+ * Safely free any heap data contained in the passed lgc_object_t
+ */
 void lgc_free(lgc_object_t* obj);
 
+/**
+ * Update the object graph to say "from now holds a reference to to".
+ *
+ * This object graph will be respected when calling lgc_free.
+ */
 void lgc_reference(lgc_object_t *from, lgc_object_t *to);
 
+/**
+ * Only useful for debugging
+ */
 void lgc_print_object(lgc_object_t*);
 
 #endif
